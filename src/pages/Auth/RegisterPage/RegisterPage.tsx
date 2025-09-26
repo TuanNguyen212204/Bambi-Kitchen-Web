1eb00854e8a7f2844cf06f165ed261e7d8d80a55
TuanNguyen212204 <tuan51463@gmail.com>
2025-09-26 09:05:03 +0700

    Update icon

--- src/pages/Auth/RegisterPage/RegisterPage.tsx
+++ src/pages/Auth/RegisterPage/RegisterPage.tsx
@@ -148,7 +148,7 @@ export function RegisterForm() {
                         onClick={() => setShowPassword((v) => !v)}
                         className="absolute right-0 top-1/2 -translate-y-1/2 text-gray-500 hover:text-gray-700"
                       >
-                        {showPassword ? <EyeOff className="w-4 h-4" /> : <Eye className="w-4 h-4" />}
+                        {showPassword ? <Eye className="w-4 h-4" /> : <EyeOff className="w-4 h-4" />}
                       </button>
                     </div>
                   </div>
